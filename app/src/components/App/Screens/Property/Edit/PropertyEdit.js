b96fa2c22e57d3bfaac37d04c16584f7642eebf1
BjornVanHoorde <bjornvanhoorde@hotmail.be>
2022-06-05 14:37:35 +0200

    edit property

--- app/src/components/App/Screens/Property/Edit/PropertyEdit.js
+++ app/src/components/App/Screens/Property/Edit/PropertyEdit.js
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import useMutation from "../../../../../core/hooks/useMutation";
 import useTitle from "../../../../../core/hooks/useTitle";
+import { PropertyRoutes, route } from "../../../../../core/routing";
 import Alert from "../../../../Design/Alert/Alert";
 import Container from "../../../../Design/Container/Container";
 import PropertyForm from "../../../Shared/Property/Form/PropertyForm";
@@ -14,7 +15,27 @@ const PropertyEdit = () => {
 
   useTitle(t("properties.edit.title"));
 
-  const handleSubmit = (values) => {};
+  const handleSubmit = (propertyValues, addressValues) => {
+    mutate(
+      `${process.env.REACT_APP_API_URL}/addresses/${propertyValues.addressId}`,
+      {
+        method: "PATCH",
+        data: addressValues,
+        onSuccess: (result) => {
+          propertyValues = { ...propertyValues, addressId: result.id };
+          mutate(`${process.env.REACT_APP_API_URL}/properties/${property.id}`, {
+            method: "PATCH",
+            data: propertyValues,
+            onSuccess: () => {
+              onUpdate();
+              navigate(route(PropertyRoutes.Detail, { id: property.id }));
+              //TODO edit/delete photos
+            },
+          });
+        },
+      }
+    );
+  };
 
   return (
     <Container>
